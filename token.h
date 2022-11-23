dfa532ab8e7204f8557afef1107bba3e7097a1e8
BryanArtega0024 <71563802+BryanArtega0024@users.noreply.github.com>
2022-11-22 21:38:54 -0500

    Delete token.h

--- token.h
+++
@@ -1,5 +0,0 @@
-const char* ssid = "*******";
-const char* password = "Arteaga06012020";
-const String token = "5440366077:AAGz1tqb8UP2FJt5fDUDUhuhH1bp3D3o4hw"; 
-const int IDchat = 2069982786;
-const String nombre = "demo_robr_bot";
